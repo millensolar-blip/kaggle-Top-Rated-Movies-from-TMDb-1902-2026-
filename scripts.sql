46fa17350afbe410cfa8eb41fbf86116027ed30f
millensolar-blip <millensolar@gmail.com>
2026-01-23 22:14:05 +0200

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -15,3 +15,34 @@ select column_name
 from information_schema.columns
 where table_schema = 'top_rated_mov_tmdb_1902_2026'
 and table_name = 'top_rated_movies';
+
+-- ======================================
+-- Cleaning Data to Silver & Gold Layers
+-- ======================================
+with base as (
+select id
+	, trim(title) as title
+	, trim(regexp_replace(overview, E'[\\r\\n]+', ' ', 'g')) as overview
+	, nullif(trim(release_date), '')::date as release_date
+	, popularity::float4 as popularity
+	, vote_average::float4 as vote_average
+	, vote_count::int as vote_count
+	, row_number() over(partition by id order by id asc) as id_rank
+from top_rated_mov_tmdb_1902_2026.top_rated_movies
+) select id
+	, title
+	, overview
+	, release_date
+	, popularity
+	, vote_average
+	, vote_count
+	, case 
+		when release_date is not null then true
+		else false
+	end as has_release_date
+	, case
+		when (row_number() over(order by popularity desc)::numeric / count(*) over() * 100)::numeric < 0.99 then true
+		else false
+	end as is_popularity_outlier
+from base
+where id_rank = 1;
